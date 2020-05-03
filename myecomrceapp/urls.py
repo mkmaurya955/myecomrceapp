1b1fdd4c5c256639aac715d69cbb4b978a8e648b
Manish kumar <mkmaurya955@gmail.com>
2020-05-03 10:45:58 +0530

    make it better

---
+++ myecomrceapp/urls.py
@@ -0,0 +1,52 @@
+"""myecomrceapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+from newsletter import views
+from carts.views import CartView, ItemCountView, CheckoutView, CheckoutFinalView
+from orders.views import (
+                    AddressSelectFormView, 
+                    UserAddressCreateView, 
+                    OrderList, 
+                    OrderDetail)
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', views.home, name='home'),
+    url(r'^contact/$', views.contact, name='contact'),
+    # url(r'^about/$', views.about, name='about'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^products/', include('products.urls')),
+    url(r'^categories/', include('products.urls_categories')),
+    url(r'^orders/$', OrderList.as_view(), name='orders'),
+    url(r'^orders/(?P<pk>\d+)/$', OrderDetail.as_view(), name='order_detail'),
+    url(r'^cart/$', CartView.as_view(), name='cart'),
+    url(r'^cart/count/$', ItemCountView.as_view(), name='item_count'),
+    url(r'^checkout/$', CheckoutView.as_view(), name='checkout'),
+    url(r'^checkout/address/$', AddressSelectFormView.as_view(), name='order_address'),
+    url(r'^checkout/address/add/$', UserAddressCreateView.as_view(), name='user_address_create'),
+    url(r'^checkout/final/$', CheckoutFinalView.as_view(), name='checkout_final'),
+
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
